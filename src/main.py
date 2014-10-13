dd9b9ca543b5c784760d8b604186bdf59715ad2d
her0e1c1 <her0e1c1@gmail.com>
2014-10-13 18:31:59 +0900

    add company and graph func

--- src/main.py
+++ src/main.py
@@ -1,13 +1,48 @@
-from flask import Flask, render_template, request, jsonify
+import os
+import datetime
+from flask import Flask, render_template, request, jsonify, abort
 import config as C
 import models
+import graph as G
 
 app = Flask(__name__)
-app.session = models.initial_session()
+session = models.initial_session()
+
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html')
+    company_list = session.query(models.Company).all()
+    return render_template('index.html', **{"company_list": company_list})
+
+
+@app.route("/company/<int:id>", methods=["GET"])
+def company(id):
+    company = session.query(models.Company).filter_by(id=id).first()
+    if not company:
+        abort(404)
+    return render_template('company.html', **{"company": company})
+
+
+@app.route("/company/<int:id>/graph", methods=["GET"])
+def graph(id):
+    company = session.query(models.Company).filter_by(id=id).first()
+    if not company:
+        abort(404)
+
+    today = datetime.date.today()
+    G.Graph.month(company, today.year, today.month)
+
+    image_dir = C.FORMAT["image_dir"].format(code=company.code)
+    image_paths = []
+    for f in os.listdir(image_dir):
+        p = os.path.join(image_dir, f)
+        p = p[p.find("/static/"):]
+        image_paths.append(p)
+
+    return render_template('graph.html', **{
+        "company": company,
+        "image_paths": image_paths,
+    })
 
 
 if __name__ == "__main__":
