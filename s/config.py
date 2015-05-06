3afba8873e14abf34ff5d19c85c25c5fe463cef8
her0e1c1 <her0e1c1@gmail.com>
2015-05-06 11:53:03 +0000

    会社情報のヘッダーを追加

--- s/config.py
+++ s/config.py
@@ -1,4 +1,5 @@
 import os
+from collections import OrderedDict
 
 HERE = os.path.dirname(__file__)
 
@@ -19,3 +20,15 @@ FORMAT = {
     "image_dir": os.path.join(GRAPH_DIR, "{code}"),
     "month": "{code}/{year}_{month}",
 }
+
+EXCEL_COMPANY_HEADER = OrderedDict([
+    ("date", '日付'),
+    ("code", 'コード'),
+    ("name", '銘柄名'),
+    ("category33", '33業種コード'),
+    ("label33", '33業種区分'),
+    ("category17", '17業種コード'),
+    ("label17", '17業種区分'),
+    ("scale", '規模コード'),
+    ("label_scale", '規模区分'),
+])
