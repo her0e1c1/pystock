1a7296eaa6cad0d7dc3fcc9d85cd3c242200daca
her0e1c1 <her0e1c1@gmail.com>
2015-07-30 04:45:02 +0900

    refactored the import company script

--- s/import_company.py
+++ s/import_company.py
@@ -1,62 +1,62 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import argparse
-from collections import OrderedDict
-import xlrd
+# coding: utf-8
+import pandas as pd
 import models
+import click
+import config as C
+from logging import getLogger
+logger = getLogger(__name__)
 
 
-def main():
-    p = argparse.ArgumentParser()
-    p.add_argument("filepath", help="""\
-$ ./import_company fiename.xls
-:note: you can download xls at http://www.tse.or.jp/market/data/listed_companies/
-""")
-    args = p.parse_args()
-    Reader(
-        filepath=args.filepath
-    ).store()
+URL = "http://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/first-d-j.xls"
 
+@click.command(
+    help="""\
+    You can download the xls at http://www.jpx.co.jp/markets/statistics-equities/misc/01.html
+    or directly {url}
+    """.format(url=URL))
+@click.argument('xls', type=click.Path(exists=True))
+def import_company(xls):
+    Reader(filepath=xls).store()
+
+
+def inverse(dct):
+    return {v: k for k, v in dct.items()}
 
 class Reader(object):
 
-    header = OrderedDict([
-        ("date", '日付'),
-        ("code", 'コード'),
-        ("name", '銘柄名'),
-        ("category33", '33業種コード'),
-        ("label33", '33業種区分'),
-        ("category17", '17業種コード'),
-        ("label17", '17業種区分'),
-        ("scale", '規模コード'),
-        ("label_scale", '規模区分'),
-     ])
-    ckeys = ["code", "name", "category17", "category33", "scale"]
+    header = C.EXCEL_COMPANY_HEADER
 
     def __init__(self, filepath):
+        excel = pd.ExcelFile(filepath)
+        if C.SHEET_NAME not in excel.sheet_names:
+            raise ValueError("The sheet %s doesn't exist in %s" % (C.SHEET_NAME, filepath))
+        sheet = excel.parse(C.SHEET_NAME)
+
+        if not self._check_header(sheet.columns):
+            raise ValueError("invalid header")
+
+        # convert japanese keys to model keys
+        self.sheet = sheet.rename(columns=inverse(self.header))
         self.filepath = filepath
-        self.book = xlrd.open_workbook(filepath)
-        self.company = models.Company
-        self.session = models.initial_session()
+        self.session = models.Session()
 
     def _check_header(self, row):
-        return [r.value for r in row] == list(self.header.values())
+        return all(row == list(self.header.values()))
 
-    def _map(self, row):
-        d = dict(zip(self.header.keys(), row))
-        return {k: v.value for k, v in d.items() if k in self.ckeys}
+    def iter(self):
+        for d in self.sheet.T.to_dict().values():
+            yield {k: v for k, v in d.items() if k in models.Company.__dict__}
 
-    def store(self, index=0):
-        self._store(self.book.sheet_by_index(index))
-
-    def _store(self, sheet):
-        assert self._check_header(sheet.row(0)), "invalid header"
-
-        for i in range(1, sheet.nrows):  # skip a header row
-            row = sheet.row(i)
-            ins = self.company(**self._map(row))
-            self.session.add(ins)
+    def store(self):
+        for data in self.iter():
+            try:
+                ins = models.Company(**data)
+            except ValueError as e:
+                logger.warn("The company whose code is %s is not imported." % data.get("code"))
+            else:
+                self.session.add(ins)
         self.session.commit()
 
 if __name__ == '__main__':
-    main()
+    import_company()
