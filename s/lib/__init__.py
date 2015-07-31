a328885c073fc31f0cf139cb98468ed20438a70f
her0e1c1 <her0e1c1@gmail.com>
2015-08-01 00:02:23 +0900

    add __init__.py using click interface

---
+++ s/lib/__init__.py
@@ -0,0 +1,44 @@
+import time
+import datetime
+
+import click
+
+from .import_company import Reader
+from .store import set_info
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.group()
+def store():
+    pass
+
+
+def mkdate(ctx, param, datestr):
+    if datestr:
+        t = time.strptime(datestr, "%Y-%m-%d")
+        d = datetime.date.fromtimestamp(time.mktime(t))
+        return d
+
+
+@click.option("--start", callback=mkdate)
+@click.option("--end", callback=mkdate)
+@click.option("--code")
+@store.command()
+def stock(code, start, end):
+    if code and start and end:
+        set_info(code, start, end)
+
+
+URL = "http://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/first-d-j.xls"
+_help="""\
+You can download the xls at http://www.jpx.co.jp/markets/statistics-equities/misc/01.html
+or directly {url}
+""".format(url=URL)
+@store.command(help=_help)
+@click.argument('xls', type=click.Path(exists=True))
+def company(xls):
+    Reader(filepath=xls).store()
