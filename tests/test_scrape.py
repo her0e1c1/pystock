af74129c5c4f983412afe86b12878895b4dd076e
her0e1c1 <her0e1c1@gmail.com>
2016-02-17 00:21:09 +0900

    split_stock_date test

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -6,5 +6,5 @@ def test_yahoo_japan(code):
     from pystock.scrape import YahooJapan
     yahoo = YahooJapan()
     assert yahoo.current_value(code) > 0
-    assert yahoo.day_info(code)
+    assert len(yahoo.split_stock_date(code, page=1)) >= 0
     assert len(yahoo.history(code, page=1)) > 0
