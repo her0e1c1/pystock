33d605d0ab1a5718a8713cb55413f91fc8245459
her0e1c1 <her0e1c1@gmail.com>
2016-02-17 11:05:19 +0900

    add company page test case

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,13 @@
 from pystock.server import app
+from pystock import query
 
 
 def test_index():
     c = app.test_client()
     assert c.get("/").status_code == 200
+
+
+def test_company():
+    c = app.test_client()
+    company = query.Company.first()
+    assert c.get("/company/%s" % company.id).status_code == 200
