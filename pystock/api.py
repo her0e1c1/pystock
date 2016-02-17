0a1166d626e459b122d51672a4e71793b4765987
her0e1c1 <her0e1c1@gmail.com>
2016-02-17 11:37:17 +0900

    rm unused code

--- pystock/api.py
+++ pystock/api.py
@@ -7,28 +7,6 @@ import pandas as pd
 from s import models
 
 
-def first_by(code=None, raise_error=False):
-    query = models.Session().query(models.Company)
-    if code:
-        query = query.filter_by(code=code)
-    ins =  query.first()
-    if not ins and raise_error:
-        raise ValueError("not exists")
-    return ins
-
-
-def _get_day_info(company_id, start, end):
-    di = models.DayInfo
-    s = models.Session()
-    query = s.query(di)
-    query = query.filter_by(company_id=company_id)
-    query = query.filter(sql.and_(
-        start <= di.date
-        #, di.date <= end
-    ))
-    return query.all()
-
-
 def list_company_which_go_down_rolling_mean(period=90, type="closing"):
     """現在、長期移動平均線を下回っている会社を返す"""
     company_list = models.Session().query(models.Company).all()
@@ -43,12 +21,6 @@ def list_company_which_go_down_rolling_mean(period=90, type="closing"):
         if (mean.tail(1) > df.closing.tail(1)).bool():
             yield company
 
-
-def str2date(s):
-    if isinstance(s, (str, unicode)):
-        return datetime.datetime.strptime(s, "%Y-%m-%d").date()
-
-
 def convert_daterange(start, end):
     if end is None:
         end = datetime.date.today()
@@ -62,22 +34,6 @@ def to_series(day_infos, type="closing"):
     # plt.plot(*result)
     return pd.DataFrame([{"date": di.date, "closing": di.closing} for di in day_infos])
     # return pd.DataFrame(zip(*[(di.date, di.closing) for di in day_infos]))
-
-
-def show(code, start, end=None):
-    """指定された会社の株価を返す"""
-    # TODO: 分割も考慮
-    start, end = convert_daterange(start, end)
-    company = first_by(code=code, raise_error=True)
-    day_infos = _get_day_info(company.id, start, end)
-    return day_infos
-    # return to_series(day_infos)
-
-
-def show1(code, start, end=None):
-    day_infos = show(code, start, end)
-    df = to_series(day_infos)
-    return df
 
 
 def show_rolling_mean(code, start, end=None, type="closing", period=30):
