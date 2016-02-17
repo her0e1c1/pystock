432dd2788656c35d6ef84d25cddc5061c11274d1
her0e1c1 <her0e1c1@gmail.com>
2016-02-17 23:23:10 +0900

    rename

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
     name='stock',
     version='0.1',
     author='Hiroyuki Ishii',
-    py_modules=['pystock'],
+    py_modules=['stock'],
     packages=find_packages(),
     description='A command line tool for stock markets',
     keywords=['stock'],
     install_requires=install_requires,
     entry_points='''
         [console_scripts]
-        pystock=pystock.cmd:cli
+        pystock=stock.cmd:cli
     ''',
 )
