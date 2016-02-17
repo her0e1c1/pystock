000f43ac20eeae6a1c0c39d9798fff8173df7908
her0e1c1 <her0e1c1@gmail.com>
2016-02-17 22:52:50 +0900

    rename package name

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='pystock',
+    name='stock',
     version='0.1',
+    author='Hiroyuki Ishii',
     py_modules=['pystock'],
     packages=find_packages(),
+    description='A command line tool for stock markets',
+    keywords=['stock'],
     install_requires=[
         'Click',
     ],
