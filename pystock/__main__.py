adaa9b7683dda3f25cd0d1c793fb56ee9523b0a3
her0e1c1 <her0e1c1@gmail.com>
2016-02-17 11:43:33 +0900

    move __main__.py

---
+++ pystock/__main__.py
@@ -0,0 +1,5 @@
+from .cmd import cli
+
+# you can run click functions like this
+# python -m pystock setup
+cli()
--- pystock/cmd/__main__.py
+++
@@ -1,5 +0,0 @@
-from pystock.cmd import cli
-
-# you can run click functions like this
-# python -m pystock.lib setup
-cli()
