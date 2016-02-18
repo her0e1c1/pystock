5a36b89b3b5c84cef220236358e4058a07db43d3
her0e1c1 <her0e1c1@gmail.com>
2016-02-18 23:34:45 +0900

    add calculate cron

--- clock.py
+++ clock.py
@@ -4,11 +4,14 @@ from logging import getLogger
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from stock import query
+from stock import service
 from stock import util
 
 logging.basicConfig(level=logging.INFO)
 logger = getLogger(__name__)
 sched = BlockingScheduler()
+
+# WARN: minus 9 if you want to use JST
 
 
 # @sched.scheduled_job('interval', minutes=1)
@@ -17,6 +20,11 @@ sched = BlockingScheduler()
 def scheduled_scrape_japan():
     logger.info('CRON: Scrape and store stock')
     query.DayInfo.sets(each=True, ignore=True, last_date=util.last_date())
+
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=15, minute=0)
+def scheduled_calculate():
+    service.closing_minus_rolling_mean_25()
 
 
 logger.info('CRON: Start ...')
