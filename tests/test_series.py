a11b6ecf6a3ef08afd388eb0cf82a2dfb9ce6528
her0e1c1 <her0e1c1@gmail.com>
2016-02-21 10:33:52 +0900

    test RSI

---
+++ tests/test_series.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from stock import wrapper
+
+
+def test_RSI():
+    def f(series, period=10):
+        series = list(series)
+        seq = wrapper.RSI(pd.Series(series), period=period)
+        return seq[period]
+
+    # 10日のデータには、差分を取るのに11日必要
+    assert f(range(11)) == 100
+    assert f(reversed(range(11))) == 0
+    assert f([0, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4]) == 70
+    assert f([0, 1, 0, 1, 0, 1, 0, 1, 2, 3, 4]) == 70
