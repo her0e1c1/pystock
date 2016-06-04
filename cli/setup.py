3d82328e1fdb6f8fe5b517751107eeef2539ff0e
her0e1c1 <her0e1c1@gmail.com>
2016-06-04 15:24:49 +0000

    remove unused library for web

---
+++ cli/setup.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_requires = [str(ir.req) for ir in parse_requirements("requirements.txt", session=False)]
+
+setup(
+    name='stock',
+    version='0.8',
+    author='Hiroyuki Ishii',
+    author_email="hiroyuki.ishii.42@gmail.com",
+    url="http://f1nance.herokuapp.com/",
+    py_modules=['stock'],
+    packages=find_packages(),
+    description='A command line tool for stock markets',
+    dependency_links=[],
+    keywords=['stock'],
+    install_requires=install_requires,
+    entry_points='''
+        [console_scripts]
+        stock=stock.cmd:cli
+    ''',
+)
