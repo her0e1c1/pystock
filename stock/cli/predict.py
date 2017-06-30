c441cc500edf9f081c423d8b21b1cb703aacb6f5
her0e1c1 <her0e1c1@gmail.com>
2017-06-30 13:36:23 +0800

    Add logs

--- stock/cli/predict.py
+++ stock/cli/predict.py
@@ -12,6 +12,7 @@ from .main import cli, AliasedGroup
 @cli.command(help="Predict prices")
 def predict():
     for (code, prices) in query.get_prices_by_code():
+        util.send_to_slack(f"Predict {code}", "#logs")
         buy_or_sell = signals.rolling_mean(prices)
         if buy_or_sell in ["BUY", "SELL"]:
             msg = f"You should {buy_or_sell} {code}"
