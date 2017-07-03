a62ee47e9aa946caa66a4977e4cbc67cb3a218ad
her0e1c1 <her0e1c1@gmail.com>
2017-07-03 20:38:33 +0800

    Refactor: use session_scope

--- stock/query.py
+++ stock/query.py
@@ -83,16 +83,13 @@ def get(quandl_code, price_type="close", from_date=None, to_date=None, chart_typ
     if to_date:
         to_date = util.str2date(to_date)
 
-    Price = models.Price
-    session = models.Session()
-    query = session.query(Price).filter_by(quandl_code=quandl_code)
-    if from_date:
-        query = query.filter(Price.date >= from_date)
-    if to_date:
-        query = query.filter(Price.date <= to_date)
-
-    df = pd.read_sql(query.statement, query.session.bind, index_col="date")  # queryの戻り値に出来る?
-    session.close()
+    with models.session_scope() as s:
+        query = s.query(models.Price).filter_by(quandl_code=quandl_code)
+        if from_date:
+            query = query.filter(models.Price.date >= from_date)
+        if to_date:
+            query = query.filter(models.Price.date <= to_date)
+        df = pd.read_sql(query.statement, query.session.bind, index_col="date")
 
     if price_type is None:
         return df
@@ -104,20 +101,6 @@ def get(quandl_code, price_type="close", from_date=None, to_date=None, chart_typ
         series = f(series)
 
     return series
-
-
-# TODO: 並列化
-# def signal(signal=None, *args, **kw):
-#     if signal is None:
-#         signal = "rolling_mean"
-#     result = collections.defaultdict(list)
-#     for code in quandl_codes():
-#         series = get(code, **kw)
-#         f = getattr(signals, signal)
-#         buy_or_sell = f(series, *args)
-#         if buy_or_sell:
-#             result[buy_or_sell].append(code)
-#     return result
 
 
 def predict(quandl_code, *args, **kw):
