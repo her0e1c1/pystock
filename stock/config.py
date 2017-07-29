6badd55397b533ac343e4e78158c6480ea7254b5
her0e1c1 <her0e1c1@gmail.com>
2017-07-29 14:05:22 +0800

    rm dead code

--- stock/config.py
+++ stock/config.py
@@ -1,12 +1,5 @@
 import os
 import logging
-from collections import OrderedDict
-
-
-# Heroku can't import path (I don't know the reason)
-# import path
-# ROOTDIR = path.Path(__file__).parent.parent
-ROOTDIR = "/".join(os.path.abspath(__file__).split("/")[:-2])
 
 DEBUG = os.environ.get("DEBUG", True)
 LOG_LEVEL = logging.INFO
@@ -19,29 +12,5 @@ CREATE_ENGINE = {
 }
 
 SLACK_URL = os.environ.get("SLACK_URL")
-
-
-# frequently changed
-COMPANY_XLS_URL = "http://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls"
-EXCEL_COMPANY_HEADER = OrderedDict([
-    ("date", u'日付'),
-    ("code", u'コード'),
-    ("name", u'銘柄名'),
-    ("_item", u'市場・商品区分'),
-    ("category33", u'33業種コード'),
-    ("label33", u'33業種区分'),
-    ("category17", u'17業種コード'),
-    ("label17", u'17業種区分'),
-    ("scale", u'規模コード'),
-    ("label_scale", u'規模区分'),
-])
-SHEET_NAME = "Sheet1"
-
-# 今日から株価を取得する期間
-DEFAULT_DAYS_PERIOD = 3 * 30
-
-
-DEFAULT_ROLLING_MEAN_RATIO = 5
-
 
 DATE_FORMATS = ["%Y/%m/%d", "%Y-%m-%d"]
